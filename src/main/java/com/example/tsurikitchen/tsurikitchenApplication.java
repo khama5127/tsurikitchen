8e14bcbb2863435e32726279281bf779b7db61c1
khama5127 <khama5127@gmail.dom>
2023-11-18 21:05:15 +0900

    アプリ開始

---
+++ src/main/java/com/example/tsurikitchen/tsurikitchenApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsurikitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tsurikitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(tsurikitchenApplication.class, args);
+	}
+
+}
